149e01d56ed1f7a61d25c1e7f4d2063ffeb68a72
Abdellatif Al Kerde <boudikerde0@gmail.com>
2022-12-13 16:35:12 +0200

    multiplies

--- problem2.js
+++ problem2.js
@@ -1,12 +1,12 @@
 var nb = document.querySelector("#nb");
 var sum = 0;
 
-nb.addEventListener("blur", function () {
-  for (let i = 1; i < parseInt(nb.value) + 1; i++) {
-    sum = sum + i;
-  }
-  alert(sum);
-});
+// nb.addEventListener("blur", function () {
+//   for (let i = 1; i < parseInt(nb.value) + 1; i++) {
+//     sum = sum + i;
+//   }
+//   alert(sum);
+// });
 
 // nb.onblur = function () {
 //   for (let i = 0; i < parseInt(nb.value) + 1; i++) {
@@ -14,3 +14,12 @@ nb.addEventListener("blur", function () {
 //   }
 //   alert(sum);
 // };
+
+nb.addEventListener("blur", function () {
+  for (let i = 0; i < parseInt(nb.value) + 1; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  alert(sum);
+});
